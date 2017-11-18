4e8cb4eeed630e67723421db2ae750bfebb02ed5
unknown <Oskar.Przystawko@gmail.com>
2017-11-18 15:30:33 +0100

    Added Main Fragment
    -Added MainFragment.class
    -Added main_fragment.xml
    -Set start screen to Main Fragment

---
+++ PartyMaker/app/src/main/java/com/kolti/lissa/partymaker/MainFragment.java
@@ -0,0 +1,26 @@
+package com.kolti.lissa.partymaker;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by t450 on 18.11.2017.
+ */
+
+public class MainFragment extends Fragment {
+
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.main_fragment, container, false);
+
+        return view;
+    }
+}
