da62bb5a5d7cfeef8a960672defd802b93556d00
unknown <Oskar.Przystawko@gmail.com>
2017-11-18 15:27:45 +0100

    Small changes
    -Deleted FAB from Main Activity
    -Added Id to content_main.xml

--- PartyMaker/app/src/main/java/com/kolti/lissa/partymaker/MainActivity.java
+++ PartyMaker/app/src/main/java/com/kolti/lissa/partymaker/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,17 +16,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.content_main, new MainFragment())
+                .addToBackStack("main_fragment")
+                .commit();
     }
 
     @Override
