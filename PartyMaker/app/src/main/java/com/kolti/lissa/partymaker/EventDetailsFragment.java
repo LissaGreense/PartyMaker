286045ab51bb5afa79d61541860a74cdbff4bf5c
unknown <Oskar.Przystawko@gmail.com>
2017-11-19 03:47:06 +0100

    Event details dialog, full description
    -Added dialog for reading full description in EventDetailsList

--- PartyMaker/app/src/main/java/com/kolti/lissa/partymaker/EventDetailsFragment.java
+++ PartyMaker/app/src/main/java/com/kolti/lissa/partymaker/EventDetailsFragment.java
@@ -1,13 +1,17 @@
 package com.kolti.lissa.partymaker;
 
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
@@ -27,10 +31,10 @@ public class EventDetailsFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.event_details_fragment, container, false);
 
-        Event event = loadEvent(MainFragment.CHOSEN_EVENT_DETAILS_ID);
+        final Event event = loadEvent(MainFragment.CHOSEN_EVENT_DETAILS_ID);
 
         TextView title = view.findViewById(R.id.event_details_title);
-        TextView description = view.findViewById(R.id.event_details_description);
+        final Button description = view.findViewById(R.id.event_details_description);
         TextView address = view.findViewById(R.id.event_details_address);
         TextView date = view.findViewById(R.id.event_details_date);
 
@@ -38,6 +42,26 @@ public class EventDetailsFragment extends Fragment {
         description.setText(event.getDescription());
         address.setText(event.getAddress());
         date.setText(event.getFormattedDate());
+
+        description.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    builder = new AlertDialog.Builder(getContext(), android.R.style.Theme_Material_Dialog_Alert);
+                } else {
+                    builder = new AlertDialog.Builder(getContext());
+                }
+                builder.setTitle("Description")
+                        .setMessage(event.getDescription() +"")
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        })
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
+            }
+        });
 
         return view;
     }
